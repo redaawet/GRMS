04f40ecfa474a016d27cb7d5c819c63a34838ab5
redaawet <33420358+redaawet@users.noreply.github.com>
2025-11-28 14:30:55 +0300

    Fix traffic station location migration import

---
+++ traffic/signals.py
@@ -0,0 +1,29 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import (
+    TrafficCountRecord,
+    TrafficSurvey,
+    recompute_cycle_summaries_for_survey,
+    recompute_survey_summary_for_survey,
+    run_auto_qc_for_survey,
+)
+
+
+@receiver(post_save, sender=TrafficCountRecord)
+def _recompute_on_count_save(sender, instance: TrafficCountRecord, **kwargs):
+    survey = instance.traffic_survey
+    if not survey:
+        return
+    run_auto_qc_for_survey(survey)
+    recompute_cycle_summaries_for_survey(survey)
+    recompute_survey_summary_for_survey(survey)
+
+
+@receiver(post_save, sender=TrafficSurvey)
+def _recompute_on_survey_approval(sender, instance: TrafficSurvey, **kwargs):
+    if instance.qa_status != "Approved":
+        return
+    run_auto_qc_for_survey(instance)
+    recompute_cycle_summaries_for_survey(instance)
+    recompute_survey_summary_for_survey(instance)
